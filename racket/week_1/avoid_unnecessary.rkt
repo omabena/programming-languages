46ad17350012d2dcd018ec60c776532cfbe53cef
Joaquin Benavides <benavides86@gmail.com>
2017-05-17 23:01:57 -0400

    Lazy loading in racket

---
+++ racket/week_1/avoid_unnecessary.rkt
@@ -0,0 +1,15 @@
+#lang racket
+
+(provide (all-defined-out))
+
+(define (slow-add x y)
+  (letrec ([slow-id (lambda (y z)
+                      (if (= 0 z)
+                          y
+                          (slow-id y (- z 1))))])
+    (+ (slow-id x 50000000) y)))
+
+(define (my-mult x y-thunk) ;; assumes x is >= 0
+  (cond [(= x 0) 0]
+        [(= x 1) (y-thunk)]
+        [#t (+ (y-thunk) (my-mult (- x 1) y-thunk))]))
