c1353d4a1b132ceec0f020948c5edc9497d29621
Joaquín Benavides <joaquin.benavides@sumup.com>
2021-01-04 10:07:00 -0300

    Macros examples

---
+++ racket/week_1.2/macros.rkt
@@ -0,0 +1,34 @@
+#lang racket
+
+(define-syntax for
+  (syntax-rules (to do)
+    [(for lo to hi do body)
+     (let ([l lo]
+           [h hi])
+       (letrec ([loop (lambda (it)
+                        (if (> it h)
+                            #t
+                            (begin body (loop (+ it 1)))))])
+         (loop l)))]))
+
+(define-syntax let2
+  (syntax-rules ()
+    [(let2 () body)
+     body]
+    [(let2 (var val) body)
+     (let ([var val]) body)]
+    [(let2 (var1 val1 var2 val2) body)
+     (let ([var1 val2])
+       (let ([var2 val2])
+         body))]))
+
+(define-syntax my-left*
+  (syntax-rules ()
+    [(my-let* () body)
+     body]
+    [(my-let* ([var0 val0]
+               [var-rest val-rest] ...)
+              body)
+     (let ([var0 val0])
+       (my-let* ([var-rest val-rest] ...)
+                body))]))
