ad7ba4e286ce7f23e4347b065075daad48b96ec2
Joaquín Benavides <joaquin.benavides@sumup.com>
2021-01-03 22:39:14 -0300

    memoization, streams, trunks files examples

---
+++ racket/week_1.2/delay_and_force.rkt
@@ -0,0 +1,27 @@
+#lang racket
+
+(define (my-delay th)
+  (mcons #f th))
+
+(define (my-force p)
+  (if (mcar p)
+      (mcdr p)
+      (begin (set-mcar! p #t)
+             (set-mcdr! p ((mcdr p)))
+             (mcdr p))))
+
+(define (slow-add x y)
+  (letrec ([slow-id (lambda (y z)
+                      (if (= 0 z)
+                          y
+                          (slow-id y (- z 1))))])
+    (+ (slow-id x 500000000) y)))
+
+(define (my-mult x y-thunk)
+  (cond [(= x 0) 0]
+        [(= x 1) (y-thunk)]
+        [#t (+ (y-thunk) (my-mult (- x 1) y-thunk))]))
+
+
+;(my-mult x (let ([p (my-delay (lambda () (slow-add 3 4)))])
+;            (lambda () (my-force p))))
--- racket/week_1.2/racket_local_binding.rkt
+++ racket/week_1.2/racket_local_binding.rkt
@@ -9,3 +9,29 @@
               (if (> tlans (car xs))
                   tlans
                   (car xs)))]))
+
+(define (silly-double x)
+  (let ([x (+ x 3)]
+        [y (+ x 2)])
+    (+ x y -5)))
+
+
+(define (silly-double_2 x)
+  (let* ([x (+ x 3)]
+         [y (+ x 2)])
+    (+ x y -8)))
+
+(define (silly-double_3 x)
+  (letrec ([y (+ x 2)]
+           [f (lambda(z) (+ z y w x))]
+           [w (+ x 7)])
+    (f - 9)))
+
+(define (silly-mod2 x)
+  (letrec
+      ([even? (lambda(x) (if (zero? x) #t (odd? (- x 1))))]
+       [odd? (lambda(x) (if (zero? x) #f (even? (- x 1))))])
+    (if (even? x) 0 1)))
+       
+       
+           
